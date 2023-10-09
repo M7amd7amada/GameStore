9452601600e2ffd408613c2669b6a6bcf8e4fad0
Mohammed Hamada <m7amd7amada@gmail.com>
2023-10-09 04:00:26 +0300

    Add License

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2023 Mohammed Hamada, GitHub: M7amd7amada.
+// Licensed under MIT license. See License.txt in the project root for license information.
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
